72075107a5117e5b5aa17b9c6ca24760f785b031
nadeshkagraffe <nadeshkagraffe@gmail.com>
2021-06-01 22:58:29 -0400

    arreglando fallas

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :pet_histories
   resources :pets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "clients#index"
+  root to: "pets#index"
 end
